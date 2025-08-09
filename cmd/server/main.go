f79af9bd5aacb0b023776e5370824f1672352cf6
plmwa <townstgak@gmail.com>
2025-08-09 16:37:42 +0900

    全体のひな型作成

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,15 +1,28 @@
 package main
 
 import (
-	"geekcamp-vol10-backend/internal/end"
 	"log"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
+
+	"geekcamp-vol10-backend/internal/end"
+	//"geekcamp-vol10-backend/internal/config"
+	//"geekcamp-vol10-backend/internal/handlers"
+	//"geekcamp-vol10-backend/internal/middleware"
+	//"geekcamp-vol10-backend/internal/repositories"
+	//"geekcamp-vol10-backend/internal/services"
 )
 
 func main() {
 	// Ginルーターを初期化
 	r := gin.Default()
+
+	r.GET("/health", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"status": "OK",
+		})
+	})
 
 	// /ping エンドポイントを定義
 	r.GET("/ping", end.Ping)
@@ -21,4 +34,22 @@ func main() {
 		// エラーが発生した場合、ログに詳細を出力してプログラムを終了する
 		log.Fatalf("サーバーの起動に失敗しました: %v", err)
 	}
+
+	/*
+	userRoutes := r.Group("/users")
+	{
+		userRoutes.GET("", userHandler.GetAll) // GET /users/
+		userRoutes.POST("", userHandler.Register) // POST /users/
+	}
+
+	monsterRoutes := r.Group("/monsters")
+	monsterRoutes.Use(authMiddleware.Authenticate) 
+	{
+		monsterRoutes.GET("", monsterHandler.GetByUID)
+	}
+
+	if err := r.Run(":8080"); err != nil {
+		log.Fatalf("サーバーの起動に失敗しました: %v", err)
+	}
+	*/
 }
