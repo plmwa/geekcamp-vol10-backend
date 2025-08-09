f3b1211740ab29ddf6b1e6d3ab304a5e3734f5a0
plmwa <townstgak@gmail.com>
2025-08-09 11:57:34 +0900

    first commit

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	// Ginルーターを初期化
+	r := gin.Default()
+
+	// /ping エンドポイントを定義
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "peng",
+		})
+	})
+	r.GET("/hello", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "Hello, World!",
+		})
+	})
+	r.GET("/bye", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "Goodbye, World!",
+		})
+	})
+
+	// サーバーをポート8080で起動
+	r.Run(":8080")
+}
